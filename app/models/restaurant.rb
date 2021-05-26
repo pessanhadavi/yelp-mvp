670ee250cbd7b474b40a3b9046892951616881f9
David Pessanha <davidppessanha@gmail.com>
2021-05-26 15:31:26 -0300

    Restaurants controller + view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
-CATEGORIES = %w(chinese italian japanese french belgian)
 class Restaurant < ApplicationRecord
+    CATEGORIES = %w(chinese italian japanese french belgian)
+    
     validates :name, :address, :category, presence: true
     validates :category, inclusion: { in: CATEGORIES }
 end
