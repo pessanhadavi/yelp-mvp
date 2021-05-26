4c3399e991f9275a9fe8b061239742c6ab75c5d2
David Pessanha <davidppessanha@gmail.com>
2021-05-26 19:12:11 -0300

    Refactored reviews controller and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,17 @@
 class ReviewsController < ApplicationController
-  
-  before_action :find_restaurant
-
-  def new
-    @review = Review.new
-  end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant # Linking restaurant id to review's restaurant_id
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'restaurants/show' # I want to render the method's path
     end
   end
 
   private
-
-  def find_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
